df6f85df72faa0f28a2c0f8d01fb372b6de0109c
CHANDRASHEKHAR MESHRAM <107882415+chandrashekhar-meshram@users.noreply.github.com>
2022-10-29 23:35:09 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import '../styles/App.css';
 
@@ -6,10 +6,40 @@ import '../styles/App.css';
 const searchArray = ["Newton", "School", "Newton School", "React", "Preact", "Node", "Mongoose", "Angular", "Vue", "Apple", "Microsoft", "Netflix", "Meta"]
 const App = () => {
 
+  const [value, setValue] = useState('');
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+    console.log(event.target.value);
+  }
   
   return (
     <div id="main">
-      hi
+      <h4>Search</h4>  
+      <div>
+        <input type='text' id="search-input" onChange={onChange}/>
+      </div>
+      <div>
+        <h4>Result</h4>
+        <ul>
+            {
+                searchArray.filter((item) => {
+                    const searchValue = value.toLowerCase();
+                    const arrayItem = item.toLocaleLowerCase();
+                    
+                    return(
+                        searchValue &&
+                        item.includes(searchValue) &&
+                        searchValue !== arrayItem
+                    )
+                })
+                .map((item, index) => (
+                    <li key={index}>{item}</li>
+                ))
+            }
+        </ul>
+       
+      </div>
     </div>
   )
 }
