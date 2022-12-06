82734180754b1b78e8c7149934e3814ca5c6e3e8
CHANDRASHEKHAR MESHRAM <107882415+chandrashekhar-meshram@users.noreply.github.com>
2022-12-07 01:43:56 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,32 @@
-import React, {useState} from 'react'
-
-import '../styles/App.css';
-
+import React, {useState} from 'react';
 
 const searchArray = ["Newton", "School", "Newton School", "React", "Preact", "Node", "Mongoose", "Angular", "Vue", "Apple", "Microsoft", "Netflix", "Meta"]
+
 const App = () => {
+  const [searchValue, setSearchValue] = useState("");
 
-  const [value, setValue] = useState('');
-
-  const onChange = (event) => {
-    setValue(event.target.value);
-  }
   
   return (
     <div id="main">
-      <h4>Search</h4>  
-      <div>
-        <input type='text' id="search-input" onChange={onChange}/>
-      </div>
-      <div>
-        <h4>Result</h4>
-        <ul>
-            {
-                searchArray.filter((item) => {
-                    const searchValue = value.toLowerCase();
-                    const arrayItem = item.toLocaleLowerCase();
-                    
-                    return(
-                        searchValue &&
-                        item.includes(searchValue) &&
-                        searchValue !== arrayItem
-                    )
-                })
-                .map((item, index) => (
-                    <li key={index}>{item}</li>
-                ))
-            }
-        </ul>
-       
-      </div>
+      <h4>Search</h4>        
+
+<input
+        type="text"
+        name="search"
+        value={searchValue}
+        onChange={e => setSearchValue(e.target.value)}
+      />
+      <ul>
+        {searchArray
+          .filter((item) => item.match(new RegExp(searchValue, "i")))
+          .map((item) => {
+            return <li key={item}>{item} </li>
+          })}
+      </ul>
     </div>
   )
 }
 
 export default App;
+
+// https://www.codingdeft.com/posts/react-filter/
